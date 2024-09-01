a268012464cd614ed16f9729732e04d02bdfeaf9
Tomas <michnovka@gmail.com>
2024-09-01 20:44:03 +0200

    Clean up code to delegate all functions to feature entity, without keeping state of its own

--- custom_components/dual_entity_light/light.py
+++ custom_components/dual_entity_light/light.py
@@ -1,9 +1,9 @@
 import voluptuous as vol
 from homeassistant.components.light import (
     LightEntity, PLATFORM_SCHEMA, ColorMode, LightEntityFeature,
-    ATTR_BRIGHTNESS, ATTR_COLOR_TEMP, ATTR_RGB_COLOR, ATTR_XY_COLOR, ATTR_HS_COLOR, ATTR_RGBW_COLOR, ATTR_RGBWW_COLOR, ATTR_COLOR_TEMP_KELVIN, ATTR_EFFECT_LIST, ATTR_EFFECT
+    ATTR_BRIGHTNESS, ATTR_COLOR_TEMP, ATTR_RGB_COLOR, ATTR_XY_COLOR, ATTR_HS_COLOR, ATTR_RGBW_COLOR, ATTR_RGBWW_COLOR, ATTR_COLOR_TEMP_KELVIN, ATTR_EFFECT_LIST, ATTR_EFFECT, ATTR_COLOR_MODE
 )
-from homeassistant.const import CONF_NAME, STATE_ON, CONF_ENTITY_ID, EVENT_HOMEASSISTANT_START
+from homeassistant.const import CONF_NAME, STATE_ON, STATE_OFF, CONF_ENTITY_ID, EVENT_HOMEASSISTANT_START
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.event import async_track_state_change_event
 from homeassistant.core import callback
@@ -43,21 +43,12 @@ class DualEntityLight(LightEntity):
         self._name = name
         self._onoff_entity = onoff_entity
         self._feature_entity = feature_entity
-        self._is_on = None
         self._attr_unique_id = entity_id if entity_id else f"{onoff_entity}_{feature_entity}_dual"
         self.entity_id = self._attr_unique_id
-        self._capabilities_configured = False
-        self._attr_supported_color_modes = {ColorMode.ONOFF}
-        self._attr_color_mode = ColorMode.ONOFF
-        self._min_color_temp_kelvin = None
-        self._max_color_temp_kelvin = None
-        self._attr_supported_features = 0
 
     async def async_added_to_hass(self) -> None:
         """Run when entity about to be added to hass."""
         await super().async_added_to_hass()
-
-        _LOGGER.debug(f"{self.entity_id}: async_added_to_hass - START")
 
         # Add listeners
         self.async_on_remove(
@@ -66,146 +57,120 @@ class DualEntityLight(LightEntity):
             )
         )
 
-        self._update_capabilities()
-
-        # Set initial state
-        self._update_state()
-
         _LOGGER.debug(f"{self.entity_id}: async_added_to_hass - DONE")
 
-    def _update_capabilities(self) -> None:
-        """Update the entity's capabilities based on the brightness entity."""
-        if self._capabilities_configured:
-            return
-
-        feature_entity_state = self.hass.states.get(self._feature_entity)
-        if feature_entity_state:
-            supported_color_modes = feature_entity_state.attributes.get('supported_color_modes', ColorMode.ONOFF)
-            self._attr_supported_color_modes = set(supported_color_modes)
-
-            supported_features = feature_entity_state.attributes.get('supported_features', 0)
-            self._attr_supported_features = supported_features
-
-            if ColorMode.COLOR_TEMP in self._attr_supported_color_modes:
-                self._min_color_temp_kelvin = feature_entity_state.attributes.get('min_color_temp_kelvin')
-                self._max_color_temp_kelvin = feature_entity_state.attributes.get('max_color_temp_kelvin')
-
-            self._capabilities_configured = True
-
-            _LOGGER.debug(f"{self.entity_id}: _update_capabilities - "
-                          f"Color modes: {', '.join(self._attr_supported_color_modes)}, "
-                          f"Supported features: {self._attr_supported_features}, "
-                          f"Min Color Temperature: {self._min_color_temp_kelvin}, "
-                          f"Max Color Temperature: {self._max_color_temp_kelvin}")
 
     @callback
     def _async_state_changed(self, event) -> None:
         _LOGGER.debug(f"{self.entity_id}: _async_state_changed - {event}")
-        self._update_capabilities()
-
-        """Handle child updates."""
-        self._update_state()
         self.async_write_ha_state()
-
-    def _update_state(self) -> None:
-        """Update the state of this light."""
-        onoff_state = self.hass.states.get(self._onoff_entity)
-        feature_entity_state = self.hass.states.get(self._feature_entity)
-        _LOGGER.debug(f"{self.entity_id}: _update_state - START")
-
-        if onoff_state is not None and feature_entity_state is not None:
-            self._is_on = onoff_state.state == STATE_ON and feature_entity_state.state == STATE_ON
-
-            self._update_attributes(feature_entity_state)
-
-            # Log the current state
-            _LOGGER.debug(f"{self.entity_id}: _update_state - "
-                          f"_is_on: {self._is_on}, "
-                          f"feature_entity_state: {feature_entity_state}")
 
     @property
     def supported_color_modes(self) -> set[ColorMode]|None:
         """Return the list of supported color modes."""
-        return self._attr_supported_color_modes
+
+        feature_entity_state = self.hass.states.get(self._feature_entity)
+        if feature_entity_state is not None:
+            feature_entity_supported_color_modes = feature_entity_state.attributes.get('supported_color_modes')
+            return set(feature_entity_supported_color_modes)
+        return None
+
+    @property
+    def supported_features(self) -> int|None:
+        """Return the list of supported features."""
+
+        feature_entity_state = self.hass.states.get(self._feature_entity)
+        if feature_entity_state is not None:
+            return feature_entity_state.attributes.get('supported_features')
+        return None
 
     @property
     def color_mode(self) -> ColorMode | None:
         """Return the current color mode."""
-        return self._attr_color_mode
+        feature_entity_state = self.hass.states.get(self._feature_entity)
+        feature_entity_color_mode = feature_entity_state.attributes.get(ATTR_COLOR_MODE)
+
+        # This can be empty for now, so in such case we should pick any value from the supported list
+        if feature_entity_color_mode is None:
+            feature_entity_supported_color_modes = self.supported_color_modes
+            feature_entity_color_mode = feature_entity_supported_color_modes.pop() if feature_entity_supported_color_modes else None
+        return feature_entity_color_mode
 
     @property
     def color_temp_kelvin(self) -> int | None:
         """Return the color temperature in Kelvin."""
-        if ColorMode.COLOR_TEMP in self._attr_supported_color_modes:
-            state = self.hass.states.get(self._feature_entity)
-            return state.attributes.get(ATTR_COLOR_TEMP_KELVIN)
-        return None
+        return self._xxx_color(ColorMode.COLOR_TEMP, ATTR_COLOR_TEMP_KELVIN)
 
     @property
     def rgb_color(self) -> tuple[int, int, int] | None:
         """Return the RGB color."""
-        if ColorMode.RGB in self._attr_supported_color_modes :
-            state = self.hass.states.get(self._feature_entity)
-            return state.attributes.get(ATTR_RGB_COLOR)
-        return None
+        return self._xxx_color(ColorMode.RGB, ATTR_RGB_COLOR)
+
 
     @property
     def xy_color(self) -> tuple[float, float] | None:
         """Return the XY color."""
-        if ColorMode.XY in self._attr_supported_color_modes :
-            state = self.hass.states.get(self._feature_entity)
-            return state.attributes.get(ATTR_XY_COLOR)
-        return None
+        return self._xxx_color(ColorMode.XY, ATTR_XY_COLOR)
+
 
     @property
     def hs_color(self) -> tuple[float, float] | None:
         """Return the HS color."""
-        if ColorMode.HS in self._attr_supported_color_modes :
-            state = self.hass.states.get(self._feature_entity)
-            return state.attributes.get(ATTR_HS_COLOR)
-        return None
+        return self._xxx_color(ColorMode.HS, ATTR_HS_COLOR)
+
 
     @property
     def rgbw_color(self) -> tuple[int, int, int, int] | None:
         """Return the RGBW color."""
-        if ColorMode.RGBW in self._attr_supported_color_modes :
-            state = self.hass.states.get(self._feature_entity)
-            return state.attributes.get(ATTR_RGBW_COLOR)
-        return None
+        return self._xxx_color(ColorMode.RGBW, ATTR_RGBW_COLOR)
+
 
     @property
     def rgbww_color(self) -> tuple[int, int, int, int, int] | None:
         """Return the RGBWW color."""
-        if ColorMode.RGBWW in self._attr_supported_color_modes :
-            state = self.hass.states.get(self._feature_entity)
-            return state.attributes.get(ATTR_RGBWW_COLOR)
+        return self._xxx_color(ColorMode.RGBWW, ATTR_RGBWW_COLOR)
+
+    def _xxx_color(self, color_mode, color_attribute):
+        if color_mode in self.supported_color_modes :
+            feature_entity_state = self.hass.states.get(self._feature_entity)
+            return feature_entity_state.attributes.get(color_attribute)
         return None
+
 
     @property
     def effect_list(self) -> list[str] | None:
         """Return effect list"""
-        if self._attr_supported_features & LightEntityFeature.EFFECT :
-            state = self.hass.states.get(self._feature_entity)
-            return state.attributes.get(ATTR_EFFECT_LIST)
+
+        feature_entity_state = self.hass.states.get(self._feature_entity)
+        _LOGGER.debug(f"{self.entity_id}: effect_list - "
+                      f"is_on: {feature_entity_state}, ")
+        if feature_entity_state.attributes.get('supported_features') & LightEntityFeature.EFFECT:
+            return feature_entity_state.attributes.get(ATTR_EFFECT_LIST)
         return None
 
     @property
     def effect(self) -> str | None:
         """Return current effect"""
-        if self._attr_supported_features & LightEntityFeature.EFFECT :
-            state = self.hass.states.get(self._feature_entity)
-            return state.attributes.get(ATTR_EFFECT)
+        feature_entity_state = self.hass.states.get(self._feature_entity)
+        if feature_entity_state.attributes.get('supported_features') & LightEntityFeature.EFFECT:
+            return feature_entity_state.attributes.get(ATTR_EFFECT)
         return None
 
     @property
     def min_color_temp_kelvin(self) -> int | None:
         """Return the minimum color temperature in Kelvin."""
-        return self._min_color_temp_kelvin
+        feature_entity_state = self.hass.states.get(self._feature_entity)
+        if feature_entity_state:
+            return feature_entity_state.attributes.get('min_color_temp_kelvin')
+        return None
 
     @property
     def max_color_temp_kelvin(self) -> int | None:
         """Return the maximum color temperature in Kelvin."""
-        return self._max_color_temp_kelvin
+        feature_entity_state = self.hass.states.get(self._feature_entity)
+        if feature_entity_state:
+            return feature_entity_state.attributes.get('max_color_temp_kelvin')
+        return None
 
     @property
     def name(self) -> str:
@@ -213,7 +178,11 @@ class DualEntityLight(LightEntity):
 
     @property
     def is_on(self) -> bool | None:
-        return self._is_on
+        on_off_entity_state = self.hass.states.get(self._onoff_entity)
+        feature_entity_state = self.hass.states.get(self._feature_entity)
+        if on_off_entity_state and feature_entity_state:
+            return on_off_entity_state.state == STATE_ON and feature_entity_state.state != STATE_OFF
+        return None
 
     @property
     def brightness(self) -> int | None:
@@ -249,32 +218,19 @@ class DualEntityLight(LightEntity):
         if len(brightness_data) > 1:  # More than just the entity_id
             await self.hass.services.async_call('light', 'turn_on', brightness_data)
 
-        _LOGGER.debug(f"{self.entity_id}: async_turn_on - {brightness_data}")
+        _LOGGER.info(f"{self.entity_id}: Turned on - {brightness_data}")
 
     async def async_turn_off(self, **kwargs) -> None:
         _LOGGER.debug(f"{self.entity_id}: async_turn_off - {kwargs}")
         await self.hass.services.async_call('light', 'turn_off', {'entity_id': self._onoff_entity})
-        _LOGGER.debug(f"{self.entity_id}: async_turn_off - DONE")
+        _LOGGER.info(f"{self.entity_id}: Turned off")
 
     async def async_update(self) -> None:
         """Fetch new state data for this light."""
-        _LOGGER.debug(f"{self.entity_id}: async_update")
+        on_off_entity_state = self.hass.states.get(self._onoff_entity)
+        feature_entity_state = self.hass.states.get(self._feature_entity)
 
-        if self._capabilities_configured:
-            onoff_entity_state = self.hass.states.get(self._onoff_entity)
-            feature_entity_state = self.hass.states.get(self._feature_entity)
-
-            _LOGGER.debug(f"{self.entity_id}: async_update - "
-                          f"is_on: {self._is_on}, "
-                          f"OnOff entity state: {onoff_entity_state}, "
-                          f"Feature entity state: {feature_entity_state}")
-
-            if onoff_entity_state and feature_entity_state:
-                self._update_attributes(feature_entity_state)
-
-    def _update_attributes(self, feature_entity_state) -> None:
-        # color mode is only updated when the light is ON
-        if feature_entity_state.state == STATE_ON:
-            self._attr_color_mode = feature_entity_state.attributes.get('color_mode', ColorMode.ONOFF)
-            _LOGGER.debug(f"{self.entity_id}: _update_attributes - {self._attr_color_mode}")
-
+        _LOGGER.debug(f"{self.entity_id}: async_update - "
+                      f"is_on: {self.is_on}, "
+                      f"OnOff entity state: {on_off_entity_state}, "
+                      f"Feature entity state: {feature_entity_state}")
